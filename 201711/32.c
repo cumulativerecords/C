dc41eb0253a7714d0d1d4b428fd0bbcee5da088d
Radix Descendit <radix@tutanota.de>
2017-11-24 23:04:26 -0800

    functions
    adding more functions

---
+++ 201711/32.c
@@ -0,0 +1,14 @@
+/* pun printing function */
+
+#include <stdio.h>
+
+void print_pun()
+{
+  printf("Pun wanted.\n");
+}
+
+int main()
+{ 
+  print_pun();
+  return 0;
+}
---
+++ 201711/33.c
@@ -0,0 +1,16 @@
+/* Function practice, no reference*/
+
+#include <stdio.h>
+
+//Don't forget to initialize a void type
+void funk()
+{
+  printf("Welcome message\n");
+}
+
+int main()
+{
+  funk();
+  return 0;
+}
+

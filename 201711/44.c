b16454f8cb1b97ab6d2076e53caeba44c773efe6
Radix Descendit <radix@tutanota.de>
2017-11-30 13:31:19 -0800

    part of program
    need the other piece, starting from "search" function

---
+++ 201711/44.c
@@ -0,0 +1,118 @@
+/* Writing a program to highlight structs with malloc and free*/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#define "readline.h"
+#define NAME_LEN 25
+
+struct part {
+  int number ;
+  char name[NAME_LEN+1];
+  int on_hand;
+  struct part *next;
+};
+
+struct part *find_part(int number);  /* points to first part */
+
+void insert(void);
+void search(void);
+void update(void);
+void print(void);
+
+/**************************************************************
+* main: Prompts the user to enter an operation code,	      *	
+*	Then calls a function to perform the requested        *
+*	action. Repeats until the user enters the	      *
+*	command 'q'. Prints an error message if the user      *
+*	enters an illegal code.				      *
+**************************************************************/
+int main(void)
+{
+  char code;
+
+  for (;;) {
+  printf("Enter operation code: ");
+  scanf(" %c", &code);
+  while (getchar() != '\n') 	/* skips to end of the line */
+    ;
+  switch (code) {
+    case 'i': insert();
+		break;
+    case 's': search();
+		break;
+    case 'u': update();
+		break;
+    case 'p': print();
+		break;
+    case 'q': return 0;
+    default: printf("Illegal code\n");
+  }
+  printf("\n");
+ }
+}
+
+/**************************************************************
+* find_partt:   Looks up a part number in the inventory       *
+*		list. Returns a pointer to the node           *
+*		containint the part number; if the part       *
+*		number is not found, returns NULL.            *
+***************************************************************/
+
+struct part *find_part(int number)
+{
+  struct part *p;
+  
+  for  (p = inventory;
+	p != NULL && number > p->number;
+	p = p->next)
+    ;
+if (p != NULL && number == p->number 
+  return p;
+return NULL;
+}
+
+/**************************************************************
+* insert: Prompts the user for information about a new        *
+*	  part and then inserts the part into the 	      *
+*	  inventory list; the list remains sorted by 	      *
+*	  part number. Prints an error message and 	      *
+*	  returns prematurely if the part already exists      *
+*	  or space could not be allocated for the part.       *
+***************************************************************/
+
+void insert(void)
+{
+  struct part *cur, *prev, *new_node;
+
+  if (new_node == NULL)  { 
+    printf("Database is full; can't add more parts.\n");
+    return;
+  }
+  
+  printf("Enter part number: ");
+  scanf("%d", &new_node->number);
+
+  for (cur = inventory, prev = NULL;
+       cur != NULL && new_node->number > cur_.number;
+       prev = cur, cur = cur->next)
+   ;
+  if (cur != NULL && new_node->number == cur._number) {
+    printf("Part already exists.\n");
+    free(new_node);
+    return;
+}
+
+  printf("Enter part name: ");
+  read_line(new_node->name, name_len);
+  PRINTF("Enter quantity on hand: ");
+  scanf("%d", &new_node->on_hnad);
+
+  new_node->next = cur;
+  if (prev == NULL)
+    inventory = new_node;
+  else
+    prev->next = new_node;
+}
+	
+	  
+//add search from p;g 437 King book here.	

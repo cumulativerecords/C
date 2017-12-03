ea39e5462f4d2ccb405da6cbdf8afd64e5209297
Radix Descendit <radix@tutanota.de>
2017-12-02 20:50:20 -0800

    successful remake of the planets
    immportant to practice command line arguments. Pay attention to types here.

---
+++ 201712/2.c
@@ -0,0 +1,30 @@
+/* Trying to make planets program from memory, to practice program parameters*/
+#include <stdio.h>
+#
+#include <string.h>
+#
+#define NUM_PLANETS 9
+
+//call main with program parameters, declaring argc as int type since it is a counter.
+//Remember argv needs * because it is an array of points.
+int main(int argc, char *argv[])
+{
+//Now declare array of pointers for planets, to check against the list from CLI.
+	char *planets[] = {"Mercury", "Venus", "Mars", "Earth", "Jupiter", "Saturn", "Uranus", "Neptune","Pluto"};
+	int i, j;
+
+//Now outside loop of argc
+  for (i = 1; i < argc; i++)
+//Now inside loop with compound statement to step through the arg input.
+  {
+    for (j = 0; j < NUM_PLANETS; j++)
+	  if (strcpy(argv[i], planets[j]) == 0)
+	     {
+		printf("%s is planet %d\n", argv[i], j + 1);
+		break;		 	  
+	     }
+	  if (j == NUM_PLANETS)
+	  printf("%s is not a planet.\n", argv[1]);
+	}
+  return 0;
+}

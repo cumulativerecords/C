93ca0e7e77b7aaab86516115da3958da111a735e
Radix Descendit <radix@tutanota.de>
2017-12-03 02:58:39 -0800

    going for speed
    missed a semicolon on decl planet array of pointers
    
    entered 1 instead of i for final print message conversion specifier.
    
    otherwise good to go

---
+++ 201712/8.c
@@ -0,0 +1,27 @@
+/*Program parameters planets drill*/
+#include <stdio.h>
+#include <string.h>
+#define NUM_PLANETS 9
+
+int main(int argc, char *argv[])
+{
+  //declare array and vars
+  char *planets[] = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"};
+  int i, j;
+  
+  //for loop to read data into the next loop for comparison
+  for (i = 1; i < argc; i++)
+  //brace for compound statement
+  {
+//second for loop to compare and print
+    for (j = 0; j < NUM_PLANETS; j++)
+      if (strcmp(argv[i], planets[j]) == 0)
+  	{ 
+	  printf("%s is planet %d.\n", argv[i], j + 1);
+	  break;
+	}
+      if (j == NUM_PLANETS)
+	printf("%s is not a planet.\n", argv[i]);
+  }
+  return 0;
+} 
